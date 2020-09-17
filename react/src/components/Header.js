c04ce361bc135419fa4b434a3ede34e5b5cb8f2b
Hazrat Ali <iloveyii@yahoo.com>
2020-09-17 23:05:02 +0200

    AppBar

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,12 +1,59 @@
-import React from 'react';
-import { AppBar, Toolbar } from '@material-ui/core';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Grid,
+  InputBase,
+  IconButton,
+  Badge,
+  makeStyles
+} from "@material-ui/core";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import SearchIcon from "@material-ui/icons/Search";
 
+const useStyles = makeStyles(theme => ({
+  searchInput: {
+    padding: theme.spacing(2),
+    '& .MuiInputBase-inputAdornedStart' : {
+        padding: theme.spacing(1),
+        opacity: 0.6,
+        '&:hover':{
+            backgroundColor: '#f1f1f1'
+        }
+    },
+    
+  }
+}));
 
 export default function Header() {
-
-    return(
-        <AppBar position="static">
-            <Toolbar></Toolbar>
-        </AppBar>
-    )
+  const classes = useStyles();
+  return (
+    <AppBar color="default" position="static">
+      <Toolbar>
+        <Grid container alignItems="center">
+          <Grid item>
+            <InputBase className={classes.searchInput} placeholder="Search" startAdornment={<SearchIcon />} />
+          </Grid>
+          <Grid item sm></Grid>
+          <Grid item>
+            <IconButton>
+              <Badge badgeContent={5} color="primary">
+                <NotificationsNoneIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={2} color="secondary">
+                <ChatBubbleOutlineIcon />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <ExitToAppIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 }
