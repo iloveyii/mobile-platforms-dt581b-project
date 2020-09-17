3838275f1b528d10a6e67107d7e97b24ed1c6fd2
Hazrat Ali <iloveyii@yahoo.com>
2020-09-17 23:25:34 +0200

    Theme apply

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -1,28 +1,55 @@
-import React from 'react';
-import {makeStyles, CssBaseline} from "@material-ui/core";
-import Sidebar from './Sidebar';
-import Header from './Header';
+import React from "react";
+import {
+  makeStyles,
+  CssBaseline,
+  createMuiTheme,
+  ThemeProvider
+} from "@material-ui/core";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { teal } from "@material-ui/core/colors";
 
-
-const useStyles = makeStyles({
-    main: {
-        paddingLeft: '320px',
-        width: '100%',
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#009688",
+      light: "#33ab9f",
+      dark: "#00695f"
+    },
+    secondary: {
+      dark: "#27632a",
+      main: "#388e3c",
+      light: "#5fa463"
+    },
+    background: {
+      default: teal[50]
     }
+  },
+  shape: {
+    borderRadius: 0
+  },
+  spacing: 10,
+  
 });
 
+const useStyles = makeStyles({
+  main: {
+    paddingLeft: "320px",
+    width: "100%"
+  }
+});
 
 function App() {
   const classes = useStyles();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Sidebar />
       <div className={classes.main}>
-        <Header/>
+        <Header />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
