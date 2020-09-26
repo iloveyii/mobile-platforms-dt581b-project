f8b94b86888f569e206f75b7f5dbcf563eb34a8c
ali <ali@softhem.se>
2020-09-27 01:46:17 +0200

    Icon import fix

--- react/src/components/ConfirmDialog.js
+++ react/src/components/ConfirmDialog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import NotListedLocationIcon from '@material-ui/icons';
+import NotListedLocationIcon from '@material-ui/icons/NotListedLocation';
 import Popup from './Popup';
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -12,7 +12,7 @@ const useStyles = makeStyles(theme => ({
             fontSize: '8rem'
         }
     }
-}))
+}));
 
 export default function ConfirmDialog(props) {
     const { showDialog, setShowDialog, action } = props;
