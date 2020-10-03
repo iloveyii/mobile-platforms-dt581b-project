df72f6ab034496675d398fa4509a14f3910174c1
Hazrat Ali <iloveyii@yahoo.com>
2020-10-03 17:45:49 +0000

    helmet morgan , 404

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,22 @@
+// ----------------------------------
+// Package Import
+// ----------------------------------
 import express from "express";
 import cors from "cors";
 import compression from "compression";
 import bodyParser from "body-parser";
 import expressLayouts from "express-ejs-layouts";
+import morgan from "morgan";
+import helmet from "helmet";
 import * as path from "path";
+
+const fileUpload = require("express-fileupload");
+
+// ----------------------------------
+// Middleware Import
+// ----------------------------------
+import { errorHandler } from "./middlewares/error_handler";
+import { notFound } from "./middlewares/not_found";
 
 // ----------------------------------
 // Routes Import
@@ -25,6 +38,20 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true}));
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(cors({origin: "*", optionsSuccessStatus: 200}));
+app.use(fileUpload({
+    createParentPath: true,
+    limits: {fileSize: 50 * 1024 * 1024},
+}));
+
+// ----------------------------------
+// Security - header
+// ----------------------------------
+app.use(helmet());
+
+// ----------------------------------
+// Logging
+// ----------------------------------
+app.use(morgan("common"));
 
 // ----------------------------------
 // EJS Layouts
@@ -38,8 +65,17 @@ app.set("view engine", "ejs");
 // ----------------------------------
 app.use("/api/v1/users", user);
 app.use("/api/v1/logins", login);
-
 app.use("/api/v1/faults", fault);
+
+// ----------------------------------
+// Not found - 404
+// ----------------------------------
+app.use(notFound);
+
+// ----------------------------------
+// Error handling
+// ----------------------------------
+app.use(errorHandler);
 
 // ----------------------------------
 // Export app
