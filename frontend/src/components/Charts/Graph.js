3399954304b0320fd54d64795d5873244f00b5ae
Hazrat Ali <iloveyii@yahoo.com>
2020-10-20 00:29:03 +0200

    Three graph works

--- frontend/src/components/Charts/Graph.js
+++ frontend/src/components/Charts/Graph.js
@@ -1,18 +1,23 @@
 import React from "react";
+import { drawChart, data } from "./functions";
 
-export default function Graph() {
+export default function Graph({ id, type, title }) {
+  setTimeout(() => {
+    drawChart(id, data);
+  }, 4000);
+
   return (
     <div className="col-md-4">
       <div className="card card-chart">
-        <div className={"card-header card-header-success"}>
-          <div className="ct-chart" id="temperatureChart"></div>
+        <div className={"card-header card-header-" + type}>
+          <div className="ct-chart" id={id + "Chart"}></div>
         </div>
         <div className="card-body">
-          <h4 className="card-title">Temperature</h4>
+          <h4 className="card-title">{title}</h4>
           <p className="card-category">
-            <span className={"text-success"} style={{ fontSize: "30px" }}>
+            <span className={"text-" + type} style={{ fontSize: "30px" }}>
               <i className="fa fa-thermometer-half" aria-hidden="true"></i>
-              <span id="temperature"></span>
+              <span id={id}></span>
             </span>
           </p>
         </div>
---
+++ frontend/src/components/Charts/functions.js
@@ -0,0 +1,85 @@
+export function drawChart(elementId, d) {
+  let labels = ["time"];
+
+  window.d = d;
+  let max = 0;
+  var series0 = [];
+  d.forEach((data) => {
+    max = max > data[elementId] ? max : data[elementId];
+    series0.push(data[elementId]);
+    labels.push(data[elementId]);
+  });
+  var series = [series0];
+  const dataTemperatureChart = {
+    labels: labels,
+    series: series,
+  };
+
+  const optionsTemperatureChart = {
+    lineSmooth: Chartist.Interpolation.cardinal({
+      tension: 0,
+    }),
+    low: 0,
+    high: max + 10, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    chartPadding: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+    },
+    showArea: true,
+    showLine: false,
+    showPoint: false,
+    fullWidth: true,
+  };
+
+  var ds = new Chartist.Line(
+    "#" + elementId + "Chart",
+    dataTemperatureChart,
+    optionsTemperatureChart
+  );
+  let historySeries = [];
+}
+
+export const data = [
+  {
+    color: "#f6cdc7",
+    eco2: 1497,
+    heading: "80.00",
+    humidity: 37,
+    pressure: 1362,
+    temperature: 1362,
+  },
+  {
+    color: "#aab0d0",
+    eco2: 708,
+    heading: "91.00",
+    humidity: 34,
+    pressure: 665,
+    temperature: 665,
+  },
+  {
+    color: "#fb4710",
+    eco2: 681,
+    heading: "8.00",
+    humidity: 24,
+    pressure: 999,
+    temperature: 999,
+  },
+  {
+    color: "#435ffc",
+    eco2: 452,
+    heading: "210.00",
+    humidity: 34,
+    pressure: 1297,
+    temperature: 1297,
+  },
+  {
+    color: "#6217a9",
+    eco2: 1011,
+    heading: "348.00",
+    humidity: 29,
+    pressure: 637,
+    temperature: 637,
+  },
+];
