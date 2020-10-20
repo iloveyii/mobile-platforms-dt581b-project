6ff800325db28f818586ca7f7d76411eb03af253
Hazrat Ali <iloveyii@yahoo.com>
2020-10-20 14:45:19 +0200

    Get permissions by email

--- backend/src/controllers/login.ts
+++ backend/src/controllers/login.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { Database } from "../models/base/Database";
 import User from "../models/User";
+import Permission from "../models/Permission";
 import Condition from "../models/base/Condition";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -8,6 +9,14 @@ import jwt from "jsonwebtoken";
 require("dotenv").config();
 
 const token_secret = process.env.TOKEN_SECRET || "this-is-a-secret";
+
+const getPermissions = async (user: any) => {
+  const condition = new Condition({ where: { email: user.email } });
+  const model = new Permission(undefined);
+  await model.read(condition);
+  console.log("Permissions list ", model.response.data);
+  return model.response.data;
+};
 
 // @desc   Make a user log in
 // @route  Post /api/v1/login
@@ -36,9 +45,10 @@ export const loginUser = async (
         { id: user.id, email: user.email },
         token_secret
       );
+      const permissions = await getPermissions(user);
       return res.status(200).send({
         success: true,
-        data: [{ id: user.id, email: user.email, token }],
+        data: [{ id: user.id, email: user.email, token, permissions }],
       });
     } else {
       return res.status(200).send({
