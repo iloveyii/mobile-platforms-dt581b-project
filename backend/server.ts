1a66e3b6c37c097f6f6d689a47b44d26619e4542
Hazrat Ali <iloveyii@yahoo.com>
2020-10-23 11:21:08 +0200

    Deploy

--- backend/server.ts
+++ backend/server.ts
@@ -4,37 +4,39 @@ import chalk from "chalk";
 import socket from "socket.io";
 
 const session = require("express-session");
+import { exec } from "child_process";
 
 import app from "./src/app";
-import {Database} from "./src/models/base/Database";
+import { Database } from "./src/models/base/Database";
 
 // ----------------------------------
 // Environment setup
 // ----------------------------------
-dotenv.config({path: ".env"});
+dotenv.config({ path: ".env" });
 const {
-    API_URL = "http://localhost",
-    API_PORT = 5500,
-    SESS_NAME = "sid",
-    SESS_SECRET = "top-secret",
-    SESS_LIFETIME = 1000 * 60 * 60 * 2, // 2 hrs
-    DB_NAME = "rdigital",
-    DB_USER = "",
-    DB_PASS = ""
+  API_URL = "http://localhost",
+  API_PORT = 5500,
+  SESS_NAME = "sid",
+  SESS_SECRET = "top-secret",
+  SESS_LIFETIME = 1000 * 60 * 60 * 2, // 2 hrs
+  DB_NAME = "rdigital",
+  DB_USER = "",
+  DB_PASS = "",
 } = process.env;
 
 app.use(
-    session({
-        name: SESS_NAME,
-        resave: false,
-        saveUninitialized: false,
-        secret: SESS_SECRET,
-        cookie: {
-            maxAge: SESS_LIFETIME,
-            sameSite: "none",
-            secure: false,
-        }
-    }));
+  session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
+    cookie: {
+      maxAge: SESS_LIFETIME,
+      sameSite: "none",
+      secure: false,
+    },
+  })
+);
 
 // ----------------------------------
 // Connect to DB
@@ -43,32 +45,50 @@ const db = new Database(DB_NAME, DB_USER, DB_PASS);
 const db_connected = db.connect();
 console.log("DB DB_NAME, DB_USER, DB_PASS", DB_NAME, DB_USER, DB_PASS);
 
-
 // ----------------------------------
 // Express server
 // ----------------------------------
 const server = app.listen(API_PORT, () => {
-    const ENV_MODE = process.env.NODE_ENV ? process.env.NODE_ENV : "prod";
-    let message = `\n${chalk.bold(`SERVER is running on ${API_URL}:${API_PORT} in ${ENV_MODE} mode `)}`;
-        message += `\n${chalk.green('To change these config(server and port), edit .env file')}`;
-        if(db_connected) {
-            message += `\n${chalk.blue('Mongodb connected to ' + DB_NAME)}`;
-        }
-        message += `\n\n${chalk.red('Press CTRL-C to stop')}`;
-        console.log(boxen(message, {
-            padding: 1,
-            borderColor: 'green',
-            margin: 1
-        }));
-    }
-);
+  const ENV_MODE = process.env.NODE_ENV ? process.env.NODE_ENV : "prod";
+  let message = `\n${chalk.bold(
+    `SERVER is running on ${API_URL}:${API_PORT} in ${ENV_MODE} mode `
+  )}`;
+  message += `\n${chalk.green(
+    "To change these config(server and port), edit .env file"
+  )}`;
+  if (db_connected) {
+    message += `\n${chalk.blue("Mongodb connected to " + DB_NAME)}`;
+  }
+  message += `\n\n${chalk.red("Press CTRL-C to stop")}`;
+  console.log(
+    boxen(message, {
+      padding: 1,
+      borderColor: "green",
+      margin: 1,
+    })
+  );
+});
 
 // ----------------------------------
 // Socket IO
 // ----------------------------------
 export const io = socket(server);
-io.on("connection", client => {
-    console.log("Made socket connection with id ", client.id);
+io.on("connection", (client) => {
+  console.log("Made socket connection with id ", client.id);
+});
+
+console.log(exec);
+
+exec("../deploy/deploy.sh", (error: any, stdout: any, stderr: any) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
 });
 
 export default server;
---
+++ deploy/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DEPLOY_DIR=../deploy
+command 2> errors.txt
+
+cd $DEPLOY_DIR
+git pull
+
+timestamp=$(date +'%Y-%m-%d %X')
+
+echo  '' >> $DEPLOY_DIR/process.log
+echo 'PROJECT (script):'$timestamp >> $DEPLOY_DIR/process.log
