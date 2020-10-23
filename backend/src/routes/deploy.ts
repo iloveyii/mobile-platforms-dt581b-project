37c6f8681e941877eb1f1d104df23c1e71657ca4
Hazrat Ali <iloveyii@yahoo.com>
2020-10-23 18:31:33 +0200

    Set route for deploy

--- backend/src/routes/deploy.ts
+++ backend/src/routes/deploy.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { createDeploy } from "../controllers/deploy";
+import { createDeploy, getDeploys } from "../controllers/deploy";
 import { authenticate_user } from "../middlewares/authenticate_user";
 import { same_user_id } from "../middlewares/same_user_id";
 import { ws_update } from "../middlewares/ws_update";
 
 const router = express.Router();
 
-router.route("/").post(createDeploy);
+router.route("/").get(getDeploys).post(createDeploy);
 
 export default router;
