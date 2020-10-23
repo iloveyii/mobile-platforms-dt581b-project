7e3454fc4c36315a6c1883cc899adc3e03a84e15
Hazrat Ali <iloveyii@yahoo.com>
2020-10-23 19:37:16 +0200

    backend serve frontend dist folder

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# Set paths
+PROJECT_DIR=/home/ubuntu/projects/dev/mobile-platforms-dt581b-project
 DEPLOY_DIR=/home/ubuntu/projects/dev/mobile-platforms-dt581b-project/deploy
+BACKEND_DIR=/home/ubuntu/projects/dev/mobile-platforms-dt581b-project/backend
+FRONTEND_DIR=/home/ubuntu/projects/dev/mobile-platforms-dt581b-project/frontend
+
+# Set error to file
 command 2> $DEPLOY_DIR/error.log
 
-cd $DEPLOY_DIR
+# Pull project repo
+cd $PROJECT_DIR
 git stash
 git pull
+
+# build frontend
+cd $FRONTEND_DIR
+npm run build
 
 timestamp=$(date +'%Y-%m-%d %X')
 
