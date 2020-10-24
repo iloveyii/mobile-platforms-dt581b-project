e6a74b00d5eaf1e0917b7a7944af4964743f2a3e
Hazrat Ali <iloveyii@yahoo.com>
2020-10-24 16:29:18 +0200

    Added project level config

---
+++ config/settings.js
@@ -0,0 +1,4 @@
+// const apiServer = "http://hkr-project.hopto.org:7700";
+// Should not be localhost but real IP of either DEV / Online server
+const apiServer = "http://10.3.141.219:7700";
+export { apiServer };
