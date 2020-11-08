904ea44625349bc9daef310b05ba8df5e51e514c
Hazrat Ali <iloveyii@yahoo.com>
2020-11-08 13:46:32 +0100

    Settings component

--- frontend/src/components/Settings/index.js
+++ frontend/src/components/Settings/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import PageHeader from "../PageHeader";
 import {
   Container,
@@ -6,54 +6,54 @@ import {
   Card,
   CardContent,
   CardMedia,
-  Typography
+  Typography,
+  TextField,
 } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/styles";
-import {Header} from "../../Layouts";
+import { Header } from "../../Layouts";
+import Form from "./Form";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   main: {
     [theme.breakpoints.up("sm")]: {
       width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth
-    }
+      marginLeft: drawerWidth,
+    },
   },
   root: {
     // marginTop: theme.spacing(1),
     display: "flex",
     marginTop: theme.spacing(5),
     "& .MuiCardMedia-root": {
-      margin: theme.spacing(2)
-    }
+      margin: theme.spacing(2),
+    },
   },
   outer: {
     display: "flex",
-    border: "none"
+    border: "none",
   },
   details: {
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
   },
   content: {
-    flex: "1 0 auto"
+    flex: "1 0 auto",
   },
   cover: {
-    width: 151
-  }
+    width: 151,
+  },
 }));
 
-
-
 function Settings(props) {
+  const classes = useStyles();
+  const { title, subtitle } = props;
+  const imageUrl = "/images/users.jpg";
 
-    const classes = useStyles();
-    const { title, subtitle, imageUrl } = props;
-
-    return (
-      <div className={classes.main}>
+  return (
+    <div className={classes.main}>
       <Header />
       <Container maxWidth="md">
         <PageHeader
@@ -64,21 +64,41 @@ function Settings(props) {
 
         <Paper className={classes.root} elevation={0}>
           <Card className={classes.outer} variant="outlined" raised={false}>
-            <CardMedia className={classes.cover} image={'imageUrl'} />
+            <CardMedia className={classes.cover} image={imageUrl} />
 
             <CardContent className={classes.details}>
               <Typography component="h5" variant="h5">
-                title
+                Personal
               </Typography>
-              <Typography variant="subtitle1" color="textSecondary">
-                sub
+              <Form />
+            </CardContent>
+          </Card>
+        </Paper>
+
+        <Paper className={classes.root} elevation={0}>
+          <Card className={classes.outer} variant="outlined" raised={false}>
+            <CardMedia className={classes.cover} image={"/images/themes.png"} />
+
+            <CardContent className={classes.details}>
+              <Typography component="h5" variant="h5">
+                Theme
               </Typography>
+              <TextField
+                margin="normal"
+                label="Name"
+                variant="outlined"
+                name="name"
+                helperText={"Enter theme name"}
+                error={false}
+                onChange={() => null}
+                value={"Light"}
+              />
             </CardContent>
           </Card>
         </Paper>
       </Container>
-      </div>
-    )
-  }
+    </div>
+  );
+}
 
 export default Settings;
